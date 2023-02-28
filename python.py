ed8639e0c6f0e7163ed9d8cbc0bdadd6253efcd0
Mukta Bhadouria <91377180+muktabhadouria@users.noreply.github.com>
2023-02-28 23:38:40 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import sys
+import random
+import time
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=random.randint(a,b)
+print(c)
+time.sleep(c)
+print("PYTHON IS THE BEST LANGUAGE IN A MILKY WAY")
+
+
+# In[ ]:
+
+
+
+
